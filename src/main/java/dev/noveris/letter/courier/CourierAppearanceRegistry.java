973eba2db4ad16ffbac888023678d7abbdb7ad34
viseriox-jpg <viseriox@gmail.com>
2026-09-12 11:38:34 -0300

    Register selectable bird courier appearances

--- src/main/java/dev/noveris/letter/courier/CourierAppearanceRegistry.java
+++ src/main/java/dev/noveris/letter/courier/CourierAppearanceRegistry.java
@@ -12,18 +12,48 @@ import java.util.Optional;
 
 public final class CourierAppearanceRegistry {
     public static final ResourceLocation DEFAULT_ID =
-            ResourceLocation.fromNamespaceAndPath(NoverisLetter.MOD_ID, "classic_mailman");
+            ResourceLocation.fromNamespaceAndPath(NoverisLetter.MOD_ID, "raven");
+    public static final ResourceLocation SPARROW_ID =
+            ResourceLocation.fromNamespaceAndPath(NoverisLetter.MOD_ID, "sparrow");
+    public static final ResourceLocation BARN_OWL_ID =
+            ResourceLocation.fromNamespaceAndPath(NoverisLetter.MOD_ID, "barn_owl");
 
     private final Map<ResourceLocation, CourierAppearance> appearances = new LinkedHashMap<>();
 
     public CourierAppearanceRegistry() {
         register(new CourierAppearance(
                 DEFAULT_ID,
-                Component.translatable("courier.noveris_letter.classic_mailman"),
+                Component.literal("Corvo"),
                 DEFAULT_ID,
-                ResourceLocation.fromNamespaceAndPath(NoverisLetter.MOD_ID, "textures/entity/courier/classic_mailman.png"),
-                ResourceLocation.fromNamespaceAndPath(NoverisLetter.MOD_ID, "textures/gui/courier/classic_mailman.png"),
+                ResourceLocation.fromNamespaceAndPath(NoverisLetter.MOD_ID, "textures/entity/courier/raven.png"),
+                ResourceLocation.fromNamespaceAndPath(NoverisLetter.MOD_ID, "textures/entity/courier/raven.png"),
                 CourierRarity.COMMON,
+                CourierMovementStyle.GROUND,
+                true,
+                false,
+                0,
+                "default"
+        ));
+        register(new CourierAppearance(
+                SPARROW_ID,
+                Component.literal("Pardal"),
+                SPARROW_ID,
+                ResourceLocation.fromNamespaceAndPath(NoverisLetter.MOD_ID, "textures/entity/courier/sparrow.png"),
+                ResourceLocation.fromNamespaceAndPath(NoverisLetter.MOD_ID, "textures/entity/courier/sparrow.png"),
+                CourierRarity.COMMON,
+                CourierMovementStyle.GROUND,
+                true,
+                false,
+                0,
+                "default"
+        ));
+        register(new CourierAppearance(
+                BARN_OWL_ID,
+                Component.literal("Coruja-das-torres"),
+                BARN_OWL_ID,
+                ResourceLocation.fromNamespaceAndPath(NoverisLetter.MOD_ID, "textures/entity/courier/barnowl.png"),
+                ResourceLocation.fromNamespaceAndPath(NoverisLetter.MOD_ID, "textures/entity/courier/barnowl.png"),
+                CourierRarity.RARE,
                 CourierMovementStyle.GROUND,
                 true,
                 false,
