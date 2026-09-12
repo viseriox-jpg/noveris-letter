52bf0cca6d86a488dc290dc8f04d74c77a9e76e7
viseriox-jpg <viseriox@gmail.com>
2026-09-12 11:53:42 -0300

    Use safe client event registration on NeoForge

--- src/main/java/dev/noveris/letter/NoverisLetter.java
+++ src/main/java/dev/noveris/letter/NoverisLetter.java
@@ -1,17 +1,14 @@
 package dev.noveris.letter;
 
 import com.mojang.logging.LogUtils;
-import dev.noveris.letter.client.CourierClient;
 import dev.noveris.letter.courier.CourierEntities;
 import dev.noveris.letter.delivery.DeliveryManager;
 import dev.noveris.letter.network.MailNetwork;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.DistExecutor;
-import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
+import net.neoforged.fml.common.Mod;
 import org.slf4j.Logger;
 
 @Mod(NoverisLetter.MOD_ID)
@@ -26,7 +23,6 @@ public final class NoverisLetter {
         NeoForge.EVENT_BUS.addListener(DeliveryManager::tick);
         NeoForge.EVENT_BUS.addListener(DeliveryManager::playerLoggedIn);
         modBus.addListener(NoverisLetter::registerPayloads);
-        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> modBus.addListener(CourierClient::registerRenderers));
     }
 
     private static void registerCommands(RegisterCommandsEvent event) {
