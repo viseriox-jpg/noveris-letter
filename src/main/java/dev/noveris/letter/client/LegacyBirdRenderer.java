45d59c6d3d1dd5721825c69f607aefd73e00edc5
viseriox-jpg <viseriox@gmail.com>
2026-09-12 18:41:51 -0300

    Fix Sparrow speech name visibility

--- src/main/java/dev/noveris/letter/client/LegacyBirdRenderer.java
+++ src/main/java/dev/noveris/letter/client/LegacyBirdRenderer.java
@@ -14,7 +14,7 @@ public final class LegacyBirdRenderer extends GeoEntityRenderer<CourierLegacyBir
     }
 
     @Override
-    protected boolean shouldShowName(CourierLegacyBirdEntity entity) {
+    public boolean shouldShowName(CourierLegacyBirdEntity entity) {
         return CourierSpeechManager.has(entity.getId());
     }
 
