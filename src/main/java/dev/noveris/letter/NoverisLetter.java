41d93219fae1bdad2ef9393d001c3272eb517ff5
viseriox-jpg <viseriox@gmail.com>
2026-09-12 11:38:47 -0300

    Register courier entities and client renderer

--- src/main/java/dev/noveris/letter/NoverisLetter.java
+++ src/main/java/dev/noveris/letter/NoverisLetter.java
@@ -1,13 +1,17 @@
 package dev.noveris.letter;
 
 import com.mojang.logging.LogUtils;
+import dev.noveris.letter.client.CourierClient;
+import dev.noveris.letter.courier.CourierEntities;
+import dev.noveris.letter.delivery.DeliveryManager;
+import dev.noveris.letter.network.MailNetwork;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.DistExecutor;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
-import dev.noveris.letter.network.MailNetwork;
-import dev.noveris.letter.delivery.DeliveryManager;
 import org.slf4j.Logger;
 
 @Mod(NoverisLetter.MOD_ID)
@@ -17,10 +21,12 @@ public final class NoverisLetter {
 
     public NoverisLetter(IEventBus modBus) {
         LOGGER.info("Initializing Noveris Letter");
+        CourierEntities.ENTITY_TYPES.register(modBus);
         NeoForge.EVENT_BUS.addListener(NoverisLetter::registerCommands);
         NeoForge.EVENT_BUS.addListener(DeliveryManager::tick);
         NeoForge.EVENT_BUS.addListener(DeliveryManager::playerLoggedIn);
         modBus.addListener(NoverisLetter::registerPayloads);
+        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> modBus.addListener(CourierClient::registerRenderers));
     }
 
     private static void registerCommands(RegisterCommandsEvent event) {
