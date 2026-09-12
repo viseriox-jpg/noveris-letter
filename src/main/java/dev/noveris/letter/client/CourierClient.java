69252963df4bfb5c8b8811ff4848ee8a9a4c0240
viseriox-jpg <viseriox@gmail.com>
2026-09-12 11:53:55 -0300

    Register courier renderer on client mod bus

--- src/main/java/dev/noveris/letter/client/CourierClient.java
+++ src/main/java/dev/noveris/letter/client/CourierClient.java
@@ -1,11 +1,17 @@
 package dev.noveris.letter.client;
 
+import dev.noveris.letter.NoverisLetter;
 import dev.noveris.letter.courier.CourierEntities;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 
+@EventBusSubscriber(modid = NoverisLetter.MOD_ID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
 public final class CourierClient {
     private CourierClient() { }
 
+    @SubscribeEvent
     public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
         event.registerEntityRenderer(CourierEntities.COURIER_BIRD.get(), CourierBirdRenderer::new);
     }
