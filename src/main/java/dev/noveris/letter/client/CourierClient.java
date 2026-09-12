ba57ccb6e4ec2b627a8133733669a75d47783e63
viseriox-jpg <viseriox@gmail.com>
2026-09-12 14:33:10 -0300

    Register renderers and model layers for all five couriers

--- src/main/java/dev/noveris/letter/client/CourierClient.java
+++ src/main/java/dev/noveris/letter/client/CourierClient.java
@@ -2,6 +2,11 @@ package dev.noveris.letter.client;
 
 import dev.noveris.letter.NoverisLetter;
 import dev.noveris.letter.courier.CourierEntities;
+import dev.noveris.letter.courier.precompiled.BoopletModel;
+import dev.noveris.letter.courier.precompiled.CapybaraModel;
+import dev.noveris.letter.courier.precompiled.CoatiModel;
+import dev.noveris.letter.courier.precompiled.MossbloomModel;
+import dev.noveris.letter.courier.precompiled.RedPandaModel;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -12,7 +17,20 @@ public final class CourierClient {
     private CourierClient() { }
 
     @SubscribeEvent
+    public static void registerLayers(EntityRenderersEvent.RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(CourierModelLayers.BOOPLET, BoopletModel::getTexturedModelData);
+        event.registerLayerDefinition(CourierModelLayers.CAPYBARA, CapybaraModel::getTexturedModelData);
+        event.registerLayerDefinition(CourierModelLayers.COATI, CoatiModel::getTexturedModelData);
+        event.registerLayerDefinition(CourierModelLayers.MOSSBLOOM, MossbloomModel::getTexturedModelData);
+        event.registerLayerDefinition(CourierModelLayers.RED_PANDA, RedPandaModel::getTexturedModelData);
+    }
+
+    @SubscribeEvent
     public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
-        event.registerEntityRenderer(CourierEntities.COURIER_BIRD.get(), CourierBirdRenderer::new);
+        event.registerEntityRenderer(CourierEntities.COURIER_BOOPLET.get(), CourierBirdRenderer::new);
+        event.registerEntityRenderer(CourierEntities.COURIER_CAPYBARA.get(), CourierBirdRenderer::new);
+        event.registerEntityRenderer(CourierEntities.COURIER_COATI.get(), CourierBirdRenderer::new);
+        event.registerEntityRenderer(CourierEntities.COURIER_MOSSBLOOM.get(), CourierBirdRenderer::new);
+        event.registerEntityRenderer(CourierEntities.COURIER_RED_PANDA.get(), CourierBirdRenderer::new);
     }
 }
