93cb1684fc2cba35d518f0798cac60fbefc9d023
viseriox-jpg <viseriox@gmail.com>
2026-09-13 04:20:31 -0300

    Add courier pickup delivery states

--- src/main/java/dev/noveris/letter/delivery/DeliveryState.java
+++ src/main/java/dev/noveris/letter/delivery/DeliveryState.java
@@ -1,10 +1,14 @@
 package dev.noveris.letter.delivery;
 
 public enum DeliveryState {
+    PICKUP_QUEUED,
+    PICKUP_PRESENTING,
+    PICKUP_WAITING,
     QUEUED,
     READY,
     PRESENTING,
     DELIVERED,
     RETRY_WAIT,
+    PICKUP_RETRY_WAIT,
     CANCELLED
 }
